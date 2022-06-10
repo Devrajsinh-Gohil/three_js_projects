21afba57ca4fe3d4376a659ebba15a2913060378
Devrajsinh <103804567+Devrajsinh03@users.noreply.github.com>
2022-06-10 11:23:53 +0530

    Added lightings and object(cube)

--- src/main.js
+++ src/main.js
@@ -4,4 +4,26 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+
+function animate() {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+}
+animate();
+
+const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+light.add( directionalLight );
+scene.add( light );
+
 renderer.render(scene, camera );
