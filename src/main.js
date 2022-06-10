7e3dcd201b1f655ae69679758e7a6cc7055c9226
Devrajsinh <103804567+Devrajsinh03@users.noreply.github.com>
2022-06-10 11:30:36 +0530

    added animation

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
@@ -14,15 +14,16 @@ camera.position.z = 5;
 
 
 function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    cube.rotation.x += 0.005;
+    cube.rotation.y += 0.005;
+    cube.rotation.z += 0.01;
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
 }
 animate();
 
 const light = new THREE.AmbientLight( 0x404040 ); // soft white light
-const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+const directionalLight = new THREE.DirectionalLight( 0x00ff00, 1 );
 light.add( directionalLight );
 scene.add( light );
 
