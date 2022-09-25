27b10e40a01212aa2ac91549ab5b28be99bfe419
Devrajsinh <103804567+Devrajsinh03@users.noreply.github.com>
2022-09-25 17:22:46 +0530

    update

--- threejsjourney/l5/05-transforms-objects/src/script.js
+++ threejsjourney/l5/05-transforms-objects/src/script.js
@@ -12,6 +12,9 @@ const scene = new THREE.Scene()
  */
 const group = new THREE.Group()
 scene.add(group)
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
 
 const cube1 = new THREE.Mesh(
     new THREE.BoxGeometry(1,1,1),
@@ -20,13 +23,15 @@ const cube1 = new THREE.Mesh(
 group.add(cube1)
 const cube2 = new THREE.Mesh(
     new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial({color:0xff0000})
+    new THREE.MeshBasicMaterial({color:0x00ff00})
 )
+cube2.position.x = -2
 group.add(cube2)
 const cube3 = new THREE.Mesh(
     new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial({color:0xff0000})
+    new THREE.MeshBasicMaterial({color:0x0000ff})
 )
+cube3.position.x = 2
 group.add(cube3)
 
 // Positions
