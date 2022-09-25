be65fce97adfbf5de72286a6410b59dde46ea524
Devrajsinh <103804567+Devrajsinh03@users.noreply.github.com>
2022-09-25 17:17:59 +0530

    update

--- threejsjourney/l5/05-transforms-objects/src/script.js
+++ threejsjourney/l5/05-transforms-objects/src/script.js
@@ -10,26 +10,42 @@ const scene = new THREE.Scene()
 /*
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const group = new THREE.Group()
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color:0xff0000})
+)
+group.add(cube1)
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color:0xff0000})
+)
+group.add(cube2)
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color:0xff0000})
+)
+group.add(cube3)
 
 // Positions
 // mesh.position.x = 0.7
 // mesh.position.y = -0.6
 // mesh.position.z = 1
 // equivalent
-mesh.position.set(0.7,-0.6,1)
+//mesh.position.set(0.7,-0.6,1)
 
 // Scale
-mesh.scale.set(2,0.5,0.5)
+//mesh.scale.set(2,0.5,0.5)
 
 // Rotation
-
+// mesh.rotation.reorder('YXZ')
+// mesh.rotation.x = Math.PI * 0.25
+// mesh.rotation.y = Math.PI * 0.25
 
 // Axes Helpers
-const axesHelper = new THREE.AxisHelper()
+const axesHelper = new THREE.AxesHelper()
 scene.add(axesHelper)
 
 /**
@@ -47,6 +63,7 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 //camera.position.set(1,1,3)
 scene.add(camera)
+
 
 /**
  * Renderer
