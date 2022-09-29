63e2e2fc585c2cb8d7edbd28917501e97581d166
Devrajsinh <103804567+Devrajsinh03@users.noreply.github.com>
2022-09-29 20:59:16 +0530

    Update script.js

--- threejsjourney/l9/09-geometries/src/script.js
+++ threejsjourney/l9/09-geometries/src/script.js
@@ -12,20 +12,37 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Object
-// const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
-const geometry = new THREE.Geometry()
+// const geometry = new THREE.BoxBufferGeometry(1, 1, 1, 2, 2, 2)
 
-const vertex1 = new THREE.Vector3(0, 0, 0)
-geometry.vertices.push(vertex1)
-const vertex2 = new THREE.Vector3(0, 1, 0)
-geometry.vertices.push(vertex2)
-const vertex3 = new THREE.Vector3(1, 0, 0)
-geometry.vertices.push(vertex3)
-const vertex4 = new THREE.Vector3(1, 1, 0)
-geometry.vertices.push(vertex4)
+// Normal Geometry
+// const geometry = new THREE.Geometry()
 
-const face = new THREE.Face3(0, 1, 2)
-geometry.faces.push(face)
+// for (let i = 0; i < 50; i++) {
+//     for (let j = 0; j < 3; j++) {
+//         geometry.vertices.push(new THREE.Vector3(
+//             (Math.random() - 0.5) * 4,
+//             (Math.random() - 0.5) * 4,
+//             (Math.random() - 0.5) * 4
+//         ))
+//     }
+//     const verticesIndex = i *3
+//     geometry.faces.push(new THREE.Face3(
+//         verticesIndex,
+//         verticesIndex + 1,
+//         verticesIndex + 2
+//         ))
+// }
+
+// Buffer Geometry
+
+const geometry = new THREE.BufferGeometry()
+const count  = 10000
+const positionsArray = new Float32Array(count * 3 * 3)
+for (let i = 0; i < count * 3 * 3; i++) {
+    positionsArray[i] = (Math.random() - 0.5) * 4
+}
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+geometry.setAttribute('position', positionsAttribute)
 
 const material = new THREE.MeshBasicMaterial({ 
     color: 0xff0000,
