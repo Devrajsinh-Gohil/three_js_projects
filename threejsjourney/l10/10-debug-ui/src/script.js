a2256f25a5e536b445a3810606d70bf7c7506333
Devrajsinh <103804567+Devrajsinh03@users.noreply.github.com>
2022-09-30 22:13:12 +0530

    update script.js

--- threejsjourney/l10/10-debug-ui/src/script.js
+++ threejsjourney/l10/10-debug-ui/src/script.js
@@ -17,7 +17,10 @@ const scene = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxBufferGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshBasicMaterial({ 
+    color: 0xff0000, 
+    // wireframe: true
+})
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
@@ -34,11 +37,11 @@ window.addEventListener('resize', () =>
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
-
+    
     // Update camera
     camera.aspect = sizes.width / sizes.height
     camera.updateProjectionMatrix()
-
+    
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -73,15 +76,36 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
-
+    
     // Update controls
     controls.update()
-
+    
     // Render
     renderer.render(scene, camera)
-
+    
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
 }
-
 tick()
+
+/**
+ * Debug
+ */
+const gui = new dat.GUI()
+// postitions
+    // method 1
+    gui.add(mesh.position, 'y', -3, 3 ,0.01).name('cube y ')
+    // method 2
+    gui.add(mesh.position, 'x').min(-3).max(3).step(0.01).name('cube x')
+    // can also be written as
+    gui
+        .add(mesh.position, 'z')
+        .min(-3)
+        .max(3)
+        .step(0.01)
+        // also can chage the name of property
+        .name('cube z')
+
+// booleans
+    gui
+        .add(mesh, 'visible')
